6de4c02601786f1023715e56d2c71d6ee02dd337
Alan Aguilar <31630572+Aslan15@users.noreply.github.com>
2020-01-21 16:52:54 -0600

    Started implementing json input support
    Goal: have a main driver program that only changes by selecting different json files

---
+++ src/CCBBA/source/inputTest.java
@@ -0,0 +1,39 @@
+package CCBBA.source;
+
+import java.io.FileReader;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class inputTest {
+
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        JSONParser parser = new JSONParser();
+
+        try {
+
+            Object obj = parser.parse(new FileReader(
+                    "src/CCBBA/inputs/inputTest.json"));
+
+            JSONObject jsonObject = (JSONObject) obj;
+
+            String name = (String) jsonObject.get("Name");
+            String author = (String) jsonObject.get("Author");
+            JSONArray companyList = (JSONArray) jsonObject.get("Company List");
+
+            System.out.println("Name: " + name);
+            System.out.println("Author: " + author);
+            System.out.println("\nCompany List:");
+            Iterator<String> iterator = companyList.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
