c456cbf973674a62ab8a1ac375a6e23bcd9e3241
Alan Aguilar <31630572+Aslan15@users.noreply.github.com>
2020-01-23 15:45:15 -0600

    Reformatted JSON input structure

--- src/CCBBA/lib/Scenario.java
+++ src/CCBBA/lib/Scenario.java
@@ -1,5 +1,6 @@
 package CCBBA.lib;
 
+import jmetal.encodings.variable.Int;
 import madkit.kernel.Watcher;
 import org.apache.commons.math3.exception.OutOfRangeException;
 import org.hipparchus.geometry.euclidean.threed.Vector3D;
@@ -13,41 +14,32 @@ public class Scenario extends Watcher {
     /**
      * environment's description
      */
+    private JSONObject scenarioData;
     private JSONArray scenarioTaskData;
     private ArrayList<Task> scenarioTasks;
     private Vector3D dimension;
 
     public Scenario(JSONObject inputData) throws Exception {
         // Load Scenario data
-        JSONArray scenarioData = (JSONArray) inputData.get("Scenario");
-
-        // Get Task List
-        int i_tasks = -1;
-        for (int i = 0; i < scenarioData.size(); i++) {
-            JSONObject object_i = (JSONObject) scenarioData.get(i);
-
-            if (object_i.containsKey("TaskList")) {
-                i_tasks = i;
-                break;
-            }
-        }
-        if(i_tasks == -1) throw new Exception("INPUT ERROR: Task List not included in input file.");
-        JSONObject taskListObject = (JSONObject) scenarioData.get(i_tasks);
-        this.scenarioTaskData = (JSONArray) taskListObject.get("TaskList");
-
+        this.scenarioData = (JSONObject) inputData.get("Scenario");
     }
 
     @Override
     protected void activate() {
         // Create tasks
-        // -Unpack Task List and create a task for each entry
+        // 1-Unpack task list
+        this.scenarioTaskData = (JSONArray) scenarioData.get("TaskList");
+
+        // 2-Create the required instances of each task on the list
         for(int i = 0; i < scenarioTaskData.size(); i++){
             JSONObject taskData = (JSONObject) scenarioTaskData.get(i);
-            int instances = (int) taskData.get("Instances");
+            int instances = Integer.parseInt( taskData.get("Instances").toString() );
 
             for(int j = 0; j < instances; j++){
                 scenarioTasks.add( new Task((JSONObject) scenarioTaskData.get(i)) );
             }
         }
+
+        int x = 1;
     }
 }
